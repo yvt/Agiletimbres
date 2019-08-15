ec6abd4c5ccdd53784dd35537cd4acb042738a3b
yvt <i@yvt.jp>
2019-08-15 15:05:26 +0900

    Fix to compile

--- Agiletimbres/TXGPDS1.h
+++ Agiletimbres/TXGPDS1.h
@@ -8,6 +8,7 @@
 
 #pragma once
 
+#include <list>
 #include "TXInstrument.h"
 
 class TPLString;
--- Agiletimbres/Utils.cpp
+++ Agiletimbres/Utils.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <string.h>
+#include <vector>
 #include "Utils.h"
 #include <tcw/twStrConv.h>
 #ifdef WIN32
